4db7c2d739f9cc0f89ce2334d6c450cd133f9d98
vlachvojta <vojtik.vlach@seznam.cz>
2024-06-24 17:51:39 +0200

    EVEN BETTER `visualize_test_step` for THE WHOLE FOLDERR, uuaaaa

--- src/visualize_test_step.py
+++ src/visualize_test_step.py
@@ -5,10 +5,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file', type=str, default='training/GraphRegressorBasicBlocksUsed_2/test_step_log_1200.tsv')
-args = parser.parse_args()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--folder', type=str, default='training-new/GraphRegressorEmbedNew')
+    return parser.parse_args()
 
+def main(args):
+    for file in os.listdir(args.folder):
+        if file.endswith('.tsv'):
+            visualize_test_step_file(os.path.join(args.folder, file))
 
 def plot_pred_vs_label(ax, x, y, title):
     ax.scatter(x, y, alpha=0.5)
@@ -22,24 +27,25 @@ def plot_pred_vs_label(ax, x, y, title):
     ax.grid(True)
 
 
-def visualize_test_step(file):
+def visualize_test_step_file(file):
+    path = os.path.dirname(file)
+    file_name = os.path.basename(file)
+    out_file = os.path.join(path, f'{file_name}.png')
+    if os.path.exists(out_file):
+        return
+
     print(f'Processing file: {file}')
     # read file
     data = pd.read_csv(file, sep='\t')
 
     fig, ax = plt.subplots(1, 2, figsize=(12, 6))
 
-
     plot_pred_vs_label(ax[0], data['fitness'], data['fitness_pred'], 'Fitness')
     plot_pred_vs_label(ax[1], data['blocks_used'], data['blocks_used_pred'], 'Blocks Used')
 
     plt.tight_layout()
-    path = os.path.dirname(file)
-    file_name = os.path.basename(file)
-
-    plt.savefig(os.path.join(path, f'{file_name}.png'))
+    plt.savefig(out_file)
     plt.clf()
 
 if __name__ == '__main__':
-    args = parser.parse_args()
-    visualize_test_step(args.file)
+    main(parse_args())
